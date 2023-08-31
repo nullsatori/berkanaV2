3cbfc458bf2a7a4abf26302ccd3fc31af2cc7cce
satori <homer2734@gmail.com>
2023-08-31 11:15:57 +0300

    fixes

--- frontend/src/pages/bogus/sichorintos.jsx
+++ frontend/src/pages/bogus/sichorintos.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "@/components/general/layout";
 import Head from "next/head";
-import HeadNav from "@/components/services/head-nav";
+import HeadNav from "@/components/general/head-nav";
 import ReactHtmlParser from "html-react-parser";
 
 function wrapUntaggedTextWithP(node) {
